bab1c4b950cf8401c46ca27b4be61ba303e8a844
NazemMoujdeen <153128002+NazemMoujdeen@users.noreply.github.com>
2025-12-14 19:17:18 +0000

    allows for server hosting

--- src/api/PokemonService.js
+++ src/api/PokemonService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/pokemon";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/pokemon";
 
 export async function savePokemon(Pokemon) {
   return await axios.post(API_URL, Pokemon);
